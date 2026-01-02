b25e466986b3805b1f5dfbc9a8c1078764be5c2c
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-02 21:15:10 +0000

    Add a function to remove outliers using the IQR method

--- ropsten-beacon-chain/tranches/data_cleaner.py
+++ ropsten-beacon-chain/tranches/data_cleaner.py
@@ -7,3 +7,23 @@ def remove_duplicates(sequence):
             seen.add(item)
             result.append(item)
     return result
+import numpy as np
+
+def remove_outliers_iqr(data, column):
+    Q1 = data[column].quantile(0.25)
+    Q3 = data[column].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    filtered_data = data[(data[column] >= lower_bound) & (data[column] <= upper_bound)]
+    return filtered_data
+
+def calculate_summary_statistics(data, column):
+    mean_val = np.mean(data[column])
+    median_val = np.median(data[column])
+    std_val = np.std(data[column])
+    return {
+        'mean': mean_val,
+        'median': median_val,
+        'std': std_val
+    }
