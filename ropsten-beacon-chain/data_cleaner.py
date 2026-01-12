26fa8f9374cd1c1ac46919f72ee8228011bbe5d8
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-12 00:24:42 +0000

    Add a function to remove duplicate entries from a list while preserving order

--- ropsten-beacon-chain/data_cleaner.py
+++ ropsten-beacon-chain/data_cleaner.py
@@ -1,8 +1,8 @@
 
-def remove_duplicates_preserve_order(iterable):
+def remove_duplicates_preserve_order(input_list):
     seen = set()
     result = []
-    for item in iterable:
+    for item in input_list:
         if item not in seen:
             seen.add(item)
             result.append(item)
