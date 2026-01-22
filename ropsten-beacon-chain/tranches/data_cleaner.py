9e78360993365150a390a7691261af2e26e6ddc3
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-22 14:33:52 +0000

    Add a function to remove outliers using the IQR method

--- ropsten-beacon-chain/tranches/data_cleaner.py
+++ ropsten-beacon-chain/tranches/data_cleaner.py
@@ -1,63 +1,11 @@
 
-import pandas as pd
 import numpy as np
-from typing import Optional, List
 
-class DataCleaner:
-    def __init__(self, df: pd.DataFrame):
-        self.df = df.copy()
-        self.original_shape = df.shape
-        
-    def remove_duplicates(self, subset: Optional[List[str]] = None) -> 'DataCleaner':
-        self.df = self.df.drop_duplicates(subset=subset, keep='first')
-        return self
-        
-    def handle_missing_values(self, strategy: str = 'drop', fill_value: Optional[float] = None) -> 'DataCleaner':
-        if strategy == 'drop':
-            self.df = self.df.dropna()
-        elif strategy == 'fill':
-            if fill_value is not None:
-                self.df = self.df.fillna(fill_value)
-            else:
-                self.df = self.df.fillna(self.df.mean())
-        return self
-        
-    def normalize_column(self, column: str) -> 'DataCleaner':
-        if column in self.df.columns:
-            col_min = self.df[column].min()
-            col_max = self.df[column].max()
-            if col_max != col_min:
-                self.df[column] = (self.df[column] - col_min) / (col_max - col_min)
-        return self
-        
-    def get_cleaned_data(self) -> pd.DataFrame:
-        return self.df
-        
-    def get_summary(self) -> dict:
-        cleaned_shape = self.df.shape
-        return {
-            'original_rows': self.original_shape[0],
-            'original_columns': self.original_shape[1],
-            'cleaned_rows': cleaned_shape[0],
-            'cleaned_columns': cleaned_shape[1],
-            'rows_removed': self.original_shape[0] - cleaned_shape[0],
-            'columns_removed': self.original_shape[1] - cleaned_shape[1]
-        }
-
-def clean_dataset(df: pd.DataFrame, 
-                  remove_dups: bool = True,
-                  handle_nulls: str = 'drop',
-                  normalize_cols: Optional[List[str]] = None) -> pd.DataFrame:
-    
-    cleaner = DataCleaner(df)
-    
-    if remove_dups:
-        cleaner.remove_duplicates()
-        
-    cleaner.handle_missing_values(strategy=handle_nulls)
-    
-    if normalize_cols:
-        for col in normalize_cols:
-            cleaner.normalize_column(col)
-    
-    return cleaner.get_cleaned_data()
+def remove_outliers_iqr(data, column):
+    Q1 = data[column].quantile(0.25)
+    Q3 = data[column].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    filtered_data = data[(data[column] >= lower_bound) & (data[column] <= upper_bound)]
+    return filtered_data
