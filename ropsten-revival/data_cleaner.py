199dd51a1ceaf37945642a716901380c547c08f2
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-24 15:02:30 +0000

    Add a function to remove outliers using the IQR method

--- ropsten-revival/data_cleaner.py
+++ ropsten-revival/data_cleaner.py
@@ -251,3 +251,13 @@ def clean_dataset(df: pd.DataFrame,
                 cleaner.fill_missing(col, strategy)
                 
     return cleaner.get_cleaned_data()
+import numpy as np
+
+def remove_outliers_iqr(data, column):
+    Q1 = data[column].quantile(0.25)
+    Q3 = data[column].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    filtered_data = data[(data[column] >= lower_bound) & (data[column] <= upper_bound)]
+    return filtered_data
