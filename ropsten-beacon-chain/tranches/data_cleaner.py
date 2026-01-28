d13cc65e78d27c91e57db3a083951451fbde16be
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-28 23:04:14 +0000

    Add data cleaning utility for removing duplicate entries from a list

--- ropsten-beacon-chain/tranches/data_cleaner.py
+++ ropsten-beacon-chain/tranches/data_cleaner.py
@@ -128,4 +128,29 @@ if __name__ == "__main__":
     key_column = sys.argv[3]
     
     success = remove_duplicates(input_file, output_file, key_column)
-    sys.exit(0 if success else 1)
+    sys.exit(0 if success else 1)def remove_duplicates(input_list):
+    seen = set()
+    result = []
+    for item in input_list:
+        if item not in seen:
+            seen.add(item)
+            result.append(item)
+    return result
+
+def clean_data_with_order(input_list, key=None):
+    if key is None:
+        key = lambda x: x
+    seen = set()
+    result = []
+    for item in input_list:
+        identifier = key(item)
+        if identifier not in seen:
+            seen.add(identifier)
+            result.append(item)
+    return result
+
+if __name__ == "__main__":
+    sample = [1, 2, 2, 3, 4, 4, 5]
+    print(remove_duplicates(sample))
+    sample_complex = [{'id': 1}, {'id': 2}, {'id': 1}, {'id': 3}]
+    print(clean_data_with_order(sample_complex, key=lambda x: x['id']))
