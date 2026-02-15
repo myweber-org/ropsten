dd5a0b266e1bbd3497f15f85f8e701781a385b80
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-15 19:15:35 +0000

    Add utility function to filter out None values from a list

--- ropsten-beacon-chain/data_cleaner.py
+++ ropsten-beacon-chain/data_cleaner.py
@@ -33,3 +33,8 @@ def clean_dataset(input_file, output_file):
 
 if __name__ == "__main__":
     clean_dataset('raw_data.csv', 'cleaned_data.csv')
+def filter_none_values(input_list):
+    """
+    Return a new list with all None values removed.
+    """
+    return [item for item in input_list if item is not None]
